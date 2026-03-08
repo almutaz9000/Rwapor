9f9f02945ab1d3e6aca49ab6eb8985cbc889f86b
almutaz9000 <almutaz9000@hotmail.com>
2026-03-08 10:23:29 +0100

    chore: Replace fragmented NaN/NA and spatial operation test scripts with consolidated versions.

--- test_nan14.R
+++ test_nan14.R
@@ -1,24 +1,26 @@
 suppressPackageStartupMessages(library(terra))
+suppressPackageStartupMessages(library(sf))
 devtools::load_all('.')
+
 v <- sf::st_read("C:/Users/almut/OneDrive/Documents/GitHub/Classification/Data/Vectors/Citrus_farms_final.geojson", quiet=TRUE)
 v <- safe_project(v, "EPSG:32636")
 urls <- wapor_generate_urls("L3-AETI-M", l3_region="JVA", period=c("2023-01-01", "2023-03-31"))
 
 r1 <- terra::rast(paste0("/vsicurl/", urls[1]))
 r1 <- terra::crop(r1, v)
-r1_c <- terra::classify(r1, cbind(NaN, NA))
-r1_m <- terra::mask(r1_c, v)
-s <- c(r1_m, r1_m)
-s_app <- terra::app(s, sum, na.rm=TRUE)
+r1_m <- terra::mask(r1, v) # Terra mask injects NaN!
+
+# Clean NaN directly to -9999
+r1_clean <- terra::classify(r1_m, cbind(NaN, -9999))
+# Clean NA directly to -9999 just in case
+r1_clean <- terra::classify(r1_clean, cbind(NA, -9999))
+
+cat("r1_clean NaN count:", terra::global(terra::app(r1_clean, is.nan), "sum")[[1]], "\n")
 
 out <- tempfile(fileext=".tif")
-terra::writeRaster(s_app, out, overwrite=TRUE, NAflag=-9999)
+terra::writeRaster(r1_clean, out, overwrite=TRUE, NAflag=-9999)
 
 r_load <- terra::rast(out)
 cat("NAflag load:", terra::NAflag(r_load), "\n")
 cat("Min/max load:", terra::minmax(r_load), "\n")
-
-# Use gdalinfo!
-gdal_info <- sf::gdal_utils("info", out, quiet=TRUE)
-cat("GDAL INFO:\n")
-cat(substr(gdal_info, 1, 1500), "\n")
+cat("NA count load:", terra::global(is.na(r_load), "sum")[[1]], "\n")
